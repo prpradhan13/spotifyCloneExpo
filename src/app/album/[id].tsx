cdf06d53e02cd7acb43de9f14e6531a1f1239477
Pritiranjan Pradhan <ppritiranjan7@gmail.com>
2024-12-08 17:03:05 +0530

    complete album details screes

--- src/app/album/[id].tsx
+++ src/app/album/[id].tsx
@@ -1,17 +1,19 @@
 import {
-  FlatList,
+  ActivityIndicator,
   Image,
+  Pressable,
   ScrollView,
-  StyleSheet,
   Text,
-  TouchableOpacity,
   View,
 } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSingleAlbum } from "@/src/utils/useSpotifyQueries";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Entypo from "@expo/vector-icons/Entypo";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
 
 const album = () => {
   const { id } = useLocalSearchParams();
@@ -20,13 +22,22 @@ const album = () => {
 
   const albumImage = albumData?.images[0]?.url;
   const albumArtistNames = albumData?.artists.map((artist: any) => artist.name);
-  const trackArtistNames = albumData?.tracks.items[0].artists.map(
-    (artist: any) => artist.name
-  );
+
+  if (isLoading) {
+    <View className="flex-1 bg-[#191414]">
+      <ActivityIndicator size={"large"} color={"#1ED760"} />
+    </View>;
+  }
+
+  if (isError) {
+    <View className="flex-1 bg-[#191414] justify-center items-center">
+      <Text className="text-white">Error: {error.message}</Text>
+    </View>;
+  }
 
   return (
     <SafeAreaView className={`bg-[#191414] flex-1 px-5 pt-5`}>
-      <View className="flex-1">
+      <ScrollView showsVerticalScrollIndicator={false} className="flex-1">
         <View className="w-full flex justify-center items-center overflow-hidden">
           {/* Image */}
           <View style={{ width: 260, height: 260, marginTop: 8 }}>
@@ -42,43 +53,85 @@ const album = () => {
             />
           </View>
 
+          {/* Album Details */}
           <View className="w-full mt-8">
-            <Text className="text-white font-bold text-2xl uppercase">
+            <Text className="text-white font-bold text-xl uppercase">
               {albumData?.name}
             </Text>
-            <Text className="text-white font-bold text-sm capitalize">
-              {albumArtistNames}
+            <Text className="text-white font-semibold text-sm capitalize mt-1">
+              {albumArtistNames?.length > 2
+                ? `${albumArtistNames.slice(0, 2).join(", ")} +${
+                    albumArtistNames?.length - 2
+                  }`
+                : albumArtistNames?.join(", ")}
             </Text>
-            <Text className="text-[#949494] font-medium text-sm capitalize">
+            <Text className="text-[#949494] font-medium text-sm capitalize mt-1">
               {albumData?.type} • {albumData?.release_date.split("-")[0]}
             </Text>
           </View>
         </View>
 
-        <FlatList
-          data={albumData?.tracks?.items}
-          keyExtractor={(flatlistId) => flatlistId.id}
-          renderItem={({ item }) => (
-            <View className="w-full mt-8 gap-5">
-              <TouchableOpacity
-                key={item?.id}
-                className="w-full flex-row justify-between"
-              >
-                <View>
-                  <Text className="text-white text-lg font-semibold">
-                    {" "}
-                    {item?.name}{" "}
-                  </Text>
-                  <Text className="text-[#949494]"> {trackArtistNames} </Text>
-                </View>
-                <View className="rotate-90">
-                  <AntDesign name="ellipsis1" size={24} color="#d1d1d1" />
-                </View>
-              </TouchableOpacity>
+        <View className="w-full flex-row mt-4 justify-between">
+          {/* Left part */}
+          <View className="w-[70%] flex-row items-center gap-6">
+            <Image
+              source={{ uri: albumImage }}
+              style={{
+                width: 40,
+                height: 50,
+                borderWidth: 2,
+                borderColor: "#cdc7c7",
+                borderRadius: 10,
+              }}
+              resizeMode="cover"
+            />
+
+            <AntDesign name="pluscircleo" size={24} color="#cdc7c7" />
+            <Feather name="arrow-down-circle" size={24} color="#cdc7c7" />
+            <MaterialCommunityIcons
+              name="dots-vertical"
+              size={26}
+              color="#cdc7c7"
+            />
+          </View>
+
+          {/* Right Part */}
+          <View className="flex-row gap-3 justify-center">
+            <View className="justify-center items-center">
+              <Entypo name="shuffle" size={24} color="#1ED760" />
             </View>
-          )}
-        />
-      </View>
+            <View className="bg-[#1ED760] w-14 h-14 rounded-full justify-center items-center">
+              <Entypo name="controller-play" size={28} color="black" />
+            </View>
+          </View>
+        </View>
+
+        {/* Music List */}
+        <View className="w-full mt-6 gap-5">
+          {albumData?.tracks?.items?.map((item: any) => (
+            <Pressable
+              key={item?.id}
+              className="w-full flex-row justify-between"
+            >
+              <View className="w-[93%]">
+                <Text className="text-white text-lg font-medium">
+                  {item?.name}
+                </Text>
+                <Text className="text-[#cdc7c7]">
+                  {item?.artists.map((artist: any) => artist.name).join(", ")}
+                </Text>
+              </View>
+              <View className="flex justify-center">
+                <MaterialCommunityIcons
+                  name="dots-vertical"
+                  size={24}
+                  color="#cdc7c7"
+                />
+              </View>
+            </Pressable>
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
